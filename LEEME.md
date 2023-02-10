fed2288472a9821b5d2f98bd432682e81af17640
Carlos Zambrano <emmanuelzzz123@gmail.com>
2023-02-09 17:10:59 -0700

    first

---
+++ LEEME.md
@@ -0,0 +1,9 @@
+npm i vite-plugin-material-ui
+npm install @mui/material @emotion/react @emotion/styled
+
+# ###### ERRORES
+
+# UseHistory
+
+npm run dev
+En el ruteo v6 cambia, ahora es UseNavigate y la const por convencion es Navigate
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navigation from "./components/Navigation";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="title"> FORMIK - EJEMPLO - TABLA </h1>
+      <Navigation />
+    </div>
+  );
+}
